fe24c4fcd5f2c45952179b7e43a99a1256648e03
Squeeks <privacymyass@gmail.com>
2011-06-21 11:47:34 +0100

    Initial commit.

---
+++ cCSVParseTests/cCSVParseTests.h
@@ -0,0 +1,17 @@
+//
+//  cCSVParseTests.m
+//  cCSVParseTests
+//
+//  Created by Squeeks on 21/06/2011.
+//  Copyright 2011 Squeeks.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface cCSVParseTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ cCSVParseTests/cCSVParseTests.m
@@ -0,0 +1,33 @@
+//
+//  cCSVParseTests.m
+//  cCSVParseTests
+//
+//  Created by Squeeks on 21/06/2011.
+//  Copyright 2011 Squeeks.
+//
+
+#import "cCSVParseTests.h"
+
+
+@implementation cCSVParseTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in cCSVParseTests");
+}
+
+@end
